9e5f7511ce39434047d413119afb83af2299b91c
Tobias Løfgren <tobloef@gmail.com>
2017-04-07 23:14:58 +0200

    Delete mysql_config.js
    So that was a mess up, but I changed the password so don't even bother trying.

--- mysql_config.js
+++
@@ -1,9 +0,0 @@
-;(function() {
-	module.exports = {
-		connectionLimit : 10,
-		host            : "localhost",
-		user            : "root",
-		password        : "mcelroymysqlpassword",
-		database        : "polychat"
-	}
-}());

c48226a0d8eca6e7d75944cf8fb6b1d03a92d6dc
Tobias Løfgren <tobloef@gmail.com>
2017-04-15 00:32:25 +0200

    Delete mysql_config.js

--- mysql_config.js
+++
@@ -1,9 +0,0 @@
-;(function() {
-	module.exports = {
-		connectionLimit : 10,
-		host            : "localhost",
-		user            : "root",
-		password        : "6VCGRHY82Aj%",
-		database        : "polychat"
-	}
-}());
